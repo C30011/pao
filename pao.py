baf7c09c090602c68498024671c21a69b6225bd5
C30011 <cleberfraga5354@gmail.com>
2025-05-12 11:56:03 -0300

    Add files via upload

---
+++ pao.py
@@ -0,0 +1,16 @@
+nome_do_reservista = (input("Digite seu nome:"))
+if nome_do_reservista == "":
+    quit()
+dia_checkin = int(input("Digite o dia do check-in:"))
+mes_checkin = int(input("Digite o mes do check-in:"))
+ano_checkin = int(input("Digite o ano do check-in:"))
+
+dia_checkout = int(input("Digite o dia do check-out:"))
+mes_checkout = int(input("Digite o mes do check-out:"))
+ano_checkout = int(input("Digite o ano do check-out:"))
+
+tipo_de_quarto = input("Tipo de quarto(Standard,Premium,Luxo)")
+
+if ano_checkout < ano_checkin:
+    or (ano_checkout == )
+
