6002b0b992674e2e5934e206277984e7c898f030
C30011 <cleberfraga5354@gmail.com>
2025-05-12 12:25:12 -0300

    Add files via upload

--- pao.py
+++ pao.py
@@ -9,8 +9,13 @@ dia_checkout = int(input("Digite o dia do check-out:"))
 mes_checkout = int(input("Digite o mes do check-out:"))
 ano_checkout = int(input("Digite o ano do check-out:"))
 
-tipo_de_quarto = input("Tipo de quarto(Standard,Premium,Luxo)")
+tipo_de_quarto = input("Tipo de quarto:(Standard,Premium,Luxo):")
 
-if ano_checkout < ano_checkin:
-    or (ano_checkout == )
+if ano_checkout < ano_checkin or \
+    (ano_checkin == ano_checkout and mes_checkout < mes_checkin) or \
+    (ano_checkin == ano_checkout and mes_checkin == mes_checkout and dia_checkout <= dia_checkin):
+    print("A data de checkout deve ser posterior da data de checkin")
+
+if mes_checkin > 12:
+    print("não exite esse mes")
 
