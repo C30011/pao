3b9267c1248bc2c05abc07d7a5070111300e00a8
C30011 <cleberfraga5354@gmail.com>
2025-05-20 10:28:48 -0300

    Update pao.py

--- pao.py
+++ pao.py
@@ -1,7 +1,8 @@
-# Entrada de dados
+#Entrada de dados
+
 nome_responsavel = input("Nome do responsável pela reserva: ")
 if not nome_responsavel:
-    print("Erro: O nome do responsável não pode estar vazio.")
+    print("O nome do responsável não pode estar vazio.")
     exit()
 
 dia_checkin = int(input("Dia do check-in: "))
@@ -12,20 +13,36 @@ dia_checkout = int(input("Dia do check-out: "))
 mes_checkout = int(input("Mês do check-out: "))
 ano_checkout = int(input("Ano do check-out: "))
 
-tipo_quarto = input("Tipo de quarto (Standard, Premium, Luxo): ")
+#Verificação
 
-# Validações
 if ano_checkout < ano_checkin or \
    (ano_checkout == ano_checkin and mes_checkout < mes_checkin) or \
-   (ano_checkout == ano_checkin and mes_checkout == mes_checkin and dia_checkout <= dia_checkin): se
-    print("Erro: A data de check-out deve ser posterior à data de check-in.")
+   (ano_checkout == ano_checkin and mes_checkout == mes_checkin and dia_checkout <= dia_checkin): 
+    print("A data de check-out deve ser posterior à data de check-in.")
     exit()
 
-if tipo_quarto not in ["Standard", "Premium", "Luxo"]:
-    print("Erro: Tipo de quarto inválido.")
+if mes_checkin and mes_checkout > 12:
+    print("Mes Invalido")
     exit()
+if mes_checkin and mes_checkout <= 0:
+    print("Mes Invalido")
+    exit()
+
+if dia_checkin and dia_checkout > 31:
+    print("Essa data não existe")
+
+if dia_checkin and dia_checkout <= 0:
+    print("Essa data não existe")
+
+# 2 parte:
+tipo_quarto = input("Tipo de quarto (Standard, Premium, Luxo): ")
+if tipo_quarto not in ["Standard", "Premium", "Luxo"]:
+    print("Tipo de quarto inválido.")
+    exit()
+
 
 # Cálculo do valor total da reserva
+    
 if tipo_quarto == "Standard":
     valor_diaria = 100
 elif tipo_quarto == "Premium":
@@ -34,7 +51,7 @@ else:  # tipo_quarto == "Luxo"
     valor_diaria = 250
 
 # Considerando que a diferença entre as datas é em dias (simplificação)
-total_dias = (ano_checkout - ano_checkin) * 365 + (mes_checkout - mes_checkin) * 30 + (dia_checkout - dia_checkin)  # Aproximação para fins do projeto
+total_dias = (ano_checkout - ano_checkin) * 360 + (mes_checkout - mes_checkin) * 30 + (dia_checkout - dia_checkin)
 valor_total = valor_diaria * total_dias
 
 # Saída de dados
@@ -42,5 +59,6 @@ print("\nDados da Reserva:")
 print(f"Responsável: {nome_responsavel}")
 print(f"Check-in: {dia_checkin}/{mes_checkin}/{ano_checkin}")
 print(f"Check-out: {dia_checkout}/{mes_checkout}/{ano_checkout}")
+print(f"Total de Dias na Reserva:{total_dias}")
 print(f"Tipo de Quarto: {tipo_quarto}")
 print(f"Valor Total: R$ {valor_total:.2f}")
